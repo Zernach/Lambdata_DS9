a643a4dc99b5bc436eb5e9d3a7070333b2e89675
Zernach <ryan@zernach.com>
2019-12-05 11:40:34 -0500

    update version number

--- Lambdata_Zernach/df_utils.py
+++ Lambdata_Zernach/df_utils.py
@@ -3,14 +3,16 @@ utility functions for working with DataFrames
 """
 
 import pandas
-TEST_DF = pandas.DataFrame([1,3,4,5,6])
+TEST_DF = pandas.DataFrame([1, 3, 4, 5, 6])
+
 
 def train_val_test_split(df):
     from sklearn.model_selection import train_test_split
-    train, test = train_test_split(df, train_size = 0.70, test_size = 0.30, random_state=42)
-    train, val = train_test_split(train, train_size = 0.50, test_size = 0.50, random_state=42)
+    train, test = train_test_split(df, train_size=0.70, test_size=0.30, random_state=42)
+    train, val = train_test_split(train, train_size=0.50, test_size=0.50, random_state=42)
     print(train.shape, val.shape, test.shape)
     return train, val, test
+
 
 def list_to_df_col(list, df):
     series = pd.Series(list)
@@ -23,11 +25,12 @@ Classes to represent games.
 
 from random import random
 
+
 class Game:
     """
     General representation of an abstract game.
     """
-    def __init__(self, rounds = 2, player1 = "Nicole", player2 = "Kumar"):
+    def __init__(self, rounds=2, player1="Nicole", player2 ="Kumar"):
         self.rounds = 2
         self.steps = 5
         self.player1 = player1
@@ -54,7 +57,7 @@ class Tic(Game):
     Tictactoe subclass of Game
     """
 
-    def __init__(self, rounds = 3, player1 = 'Superman', player2 = 'Stephanie'):
+    def __init__(self, rounds=3, player1='Superman', player2='Stephanie'):
         super().__init__(rounds, player1, player2)
 
     def print_players(self):
